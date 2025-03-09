e9acc8860d7a47db2d6f6514e5a2b8b1d014cfcc
GC_12 <156052262+ChauhanGauri@users.noreply.github.com>
2025-03-09 17:51:08 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no swaps, array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            arr[i] = scan.nextInt();
+        }
+
+        scan.close();
+
+        System.out.println("\nOriginal array: ");
+        printArray(arr);
+
+        bubbleSort(arr, n);
+
+        System.out.println("Sorted array: ");
+        printArray(arr);
+    }
+}
