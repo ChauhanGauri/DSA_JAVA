b10188d2285bd89fcff7304a7b7d7a21288fdde8
GC_12 <156052262+ChauhanGauri@users.noreply.github.com>
2025-05-21 09:49:46 +0530

    Update printCousins.java

--- Trees/printCousins.java
+++ Trees/printCousins.java
@@ -55,62 +55,63 @@ class Result {
         System.out.println(-1);
     }
 }
-// class Node
-// {
-//   int data; // data used as key value
-//   Node leftChild;
-//   Node rightChild;
-//   public Node()
-//   {
-//     data=0;
-//   }
-//   public Node(int d)
-//   {
-//     data=d;
-//   }
-// }
+/*class Node
+{
+  int data; // data used as key value
+  Node leftChild;
+  Node rightChild;
+  public Node()
+  {
+    data=0;
+  }
+  public Node(int d)
+  {
+    data=d;
+  }
+}
 
-// class Main
-// {
-//   static void inOrder(Node root)
-//   {
-//     if (root != null)
-//     {
-//       inOrder(root.leftChild);
-//       System.out.print(root.data + " ");
-//       inOrder(root.rightChild);
-//     }
-//   }
-//   static Node insertLevelOrder(int arr[], Node root, int i, int n)
-//   {
-//     if (i < n)
-//     {
-//       Node temp = new Node(arr[i]);
-//       root = temp;
-//       root.leftChild = insertLevelOrder(arr, root.leftChild, 2 * i + 1, n);
-//       root.rightChild = insertLevelOrder(arr, root.rightChild, 2 * i + 2, n);
-//     }
-//     return root;
-//   }
+class Main
+{
+  static void inOrder(Node root)
+  {
+    if (root != null)
+    {
+      inOrder(root.leftChild);
+      System.out.print(root.data + " ");
+      inOrder(root.rightChild);
+    }
+  }
+  static Node insertLevelOrder(int arr[], Node root, int i, int n)
+  {
+    if (i < n)
+    {
+      Node temp = new Node(arr[i]);
+      root = temp;
+      root.leftChild = insertLevelOrder(arr, root.leftChild, 2 * i + 1, n);
+      root.rightChild = insertLevelOrder(arr, root.rightChild, 2 * i + 2, n);
+    }
+    return root;
+  }
 
-//   static Node buildTree(int t[], int n)
-//   {
-//     Node node = null;
-//     node = insertLevelOrder(t, node, 0, n);
-//     return node;
-//   }
-//   public static void main(String[] args)
-//   {
-//     int a[]=new int[50];
-//     int n, i;
-//     Scanner s=new Scanner(System.in);
-//     n = Integer.parseInt(s.nextLine());
-//     for(i=0;i<n;i++)
-//       a[i] = s.nextInt();
-//     i = s.nextInt();
-//     Node root = null;
-//     if(n!=0)
-//       root = buildTree(a, n);
-//     Result.printCousins(root, i);
-//   }
-// }
+  static Node buildTree(int t[], int n)
+  {
+    Node node = null;
+    node = insertLevelOrder(t, node, 0, n);
+    return node;
+  }
+  public static void main(String[] args)
+  {
+    int a[]=new int[50];
+    int n, i;
+    Scanner s=new Scanner(System.in);
+    n = Integer.parseInt(s.nextLine());
+    for(i=0;i<n;i++)
+      a[i] = s.nextInt();
+    i = s.nextInt();
+    Node root = null;
+    if(n!=0)
+      root = buildTree(a, n);
+    Result.printCousins(root, i);
+  }
+}
+*/
